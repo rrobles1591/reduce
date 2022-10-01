bdb3c796da072f1a5e9e34a13bc47f6f519e24c3
root <root@Phoebe.localdomain>
2022-09-30 23:45:29 -0400

    reduce

---
+++ app.js
@@ -0,0 +1,39 @@
+function extractValue(arr, key){
+    return arr.reduce(function(acc,next){
+        acc.push(next[key]);
+        return acc;
+    },[]);
+}
+
+function vowelCount(str){
+    const vowels = "aeiou";
+    return str.split('').reduce(function(acc,next){
+        let lowerCased = next.toLowerCase()
+        if(vowels.indexOf(lowerCased) !== -1){
+            if(acc[lowerCased]){
+                acc[lowerCased]++;
+            } else {
+                acc[lowerCased] = 1;
+            }
+        }
+        return acc;
+    }, {});
+}
+
+function addKeyAndValue(arr, key, value){
+    return arr.reduce(function(acc,next,idx){
+        acc[idx][key] = value;
+        return acc;
+    },arr);
+}
+
+function partition(arr, cb){
+    return arr.reduce(function(acc,next){
+        if(cb(next)){
+            acc[0].push(next);
+        } else {
+            acc[1].push(next);
+        }
+        return acc;
+    }, [[],[]]);
+}
